2ecbd7365105392e3344d5c79c2607a6de9775f6
Praise Freedom Dike <dikepraise119@gmail.com>
2023-04-14 04:42:06 +0100

    change skill image background

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Skills({ image, name }) {
   return (
     <section>
-      <div className="lg:max-w-sm p-6  flex justify-center ">
+      <div className="lg:max-w-sm p-6 flex justify-center bg-gray-900">
         <img src={image} alt={name} className="h-10" />
       </div>
     </section>
